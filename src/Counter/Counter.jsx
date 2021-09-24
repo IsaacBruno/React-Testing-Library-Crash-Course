342863c67869ef8082dfb2b3d98577f4e2be89cf
Isaac Bruno <isaacwinfor@gmail.com>
2021-09-24 17:09:19 -0300

    feat: ensure subtract button renders with -

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -7,8 +7,9 @@ function Counter() {
     <div>
       <h3 data-testid="header">My Counter</h3>
       <h2 data-testid="counter">{counterValue}</h2>
+      <button data-testid="subtract-btn">-</button>
     </div>
   );
 }
 
-export default Counter
+export default Counter;
