d6251a230be4cd1dd2d6787bbbb0c3c0ebe3b76a
Isaac Bruno <isaacwinfor@gmail.com>
2021-09-24 17:19:49 -0300

    feat: ensure input contains initial value of 1

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
+import './Counter.css';
 
 function Counter() {
   const [counterValue, setCounterValue] = useState(0)
+  const [inputValue, setInputValue] = useState(1)
 
   return (
     <div>
       <h3 data-testid="header">My Counter</h3>
       <h2 data-testid="counter">{counterValue}</h2>
       <button data-testid="subtract-btn">-</button>
+      <input
+        data-testid="input"
+        type="number"
+        value={inputValue}
+        className='text-center'
+      />
       <button data-testid="add-btn">+</button>
     </div>
   );
