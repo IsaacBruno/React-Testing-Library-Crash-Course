548c3e5c2bfffe670280f183c9c39e189a3edddb
Isaac Bruno <isaacwinfor@gmail.com>
2021-09-24 17:06:22 -0300

    test: ensure counter initially start with text of 0

--- src/Counter/__test__/Counter.test.js
+++ src/Counter/__test__/Counter.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Counter from '../Counter';
-import { render } from '@testing-library/react';
+import { getElementError, getNodeText, render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 test('header renders with correct text', () => {
@@ -8,4 +8,11 @@ test('header renders with correct text', () => {
   const headerEl = getByTestId('header');
 
   expect(headerEl.textContent).toBe('My Counter');
+});
+
+test('counter initially start with text of 0', () => {
+  const { getByTestId } = render(<Counter />);
+  const counterEl = getByTestId('counter');
+
+  expect(counterEl.textContent).toBe('0');
 });
