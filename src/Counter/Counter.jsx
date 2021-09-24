a229c67d09bd3747c4c21b1c28b4e09118e6435d
Isaac Bruno <isaacwinfor@gmail.com>
2021-09-24 17:06:15 -0300

    feat: ensure counter initially start with text of 0

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Counter() {
+  const [counterValue, setCounterValue] = useState(0)
+
   return (
     <div>
       <h3 data-testid="header">My Counter</h3>
+      <h2 data-testid="counter">{counterValue}</h2>
     </div>
   );
 }
